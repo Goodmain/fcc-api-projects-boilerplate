8324ffc93cbc413e7447aee8b883423049aebbe3
Vitaly Iskrin <viskrin@ronasit.com>
2022-08-21 18:58:37 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/api/:date", function (req, res) {
+  const dateParam = req.params.date;
+  const date = new Date(+dateParam || dateParam);
+
+  if (!isNaN(date)) {
+    res.json({
+      unix: date.valueOf(),
+      utc: date.toUTCString()
+    });
+  } else {
+    res.sendStatus(422);
+  }
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
